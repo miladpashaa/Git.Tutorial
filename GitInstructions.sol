75c4f0b5c4bef911612e20007b42cdd14d0f703e
milad pasha <miladzanos@gmail.com>
2024-03-18 19:56:04 +0330

    git roles

---
+++ GitInstructions.sol
@@ -0,0 +1,35 @@
+/*
+cd ~<folderlocation>
+ls => git location
+git init => initialize
+git add <filename> => it put the file on stage.
+git add ./ -A => put all untrack file on stage.
+git commit -m <filename> which is on stage => creating a version by an optional name for it.
+git commit -m <filename> --amend  which was commited => changing the optional name of the file or add any thing to commited file.
+git log => showing all file in master branch.
+ 
+
+ ---------------------------visualize git & git fundementals---------------------------------
+ git reste <filename> => unstage the file.
+ git reset . => all files' in folder unstaged.
+ git checkout -- <filename> => unstage the file
+ git checkout -- . => unstage all folder or files 
+ 
+
+ 
+*/
+
+/*
+jadii
+git diff Head => shows where is head and whats change.
+git diff --staged => shows the stage files change with previous file.
+git reset <file name> => unstage a stage file
+git restore ./ _A/ <file name> => wipped out the modified file/ unstaged file.
+
+git branch => shows all branch and which branch is active at the moment
+git checkout <inactive branch> =>swop in the inactive branch
+git merge <the branch name> merge branch with the master branch
+
+ 
+
+*/
