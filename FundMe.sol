ffae4a2abdd674444700136e2c039ccaaa885b97
milad pasha <miladzanos@gmail.com>
2024-03-18 20:57:46 +0330

    fund01

---
+++ FundMe.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+contract fundme{
+    uint public minimumdonate;
+    address public owner;
+ 
+ function getprice(uint _minimum)public returns(uint){
+
+    require(msg.vslue < 0.2,"not enough donate");
+
+
+
+ }
+}
