02144fd0a5ea27e5ccdac6f4714bc7f9ae3af711
milad pasha <miladzanos@gmail.com>
2024-03-24 23:28:57 +0330

    NewRoles01

--- GitInstructions.sol
+++ GitInstructions.sol
@@ -3,18 +3,41 @@ cd ~<folderlocation>
 ls => git location
 git init => initialize
 git add <filename> => it put the file on stage.
-git add ./ -A => put all untrack file on stage.
+git add ./ -A/ folder=> put all untrack file on stage.
 git commit -m <filename> which is on stage => creating a version by an optional name for it.
 git commit -m <filename> --amend  which was commited => changing the optional name of the file or add any thing to commited file.
 git log => showing all file in master branch.
  
-
+git config --global user.name <"your name">
+git config --global user.email <"email address">
  ---------------------------visualize git & git fundementals---------------------------------
  git reste <filename> => unstage the file.
  git reset . => all files' in folder unstaged.
  git checkout -- <filename> => unstage the file
  git checkout -- . => unstage all folder or files 
  
+----------------GitHub = Specifically Designed for Git Repositories---------------------
+*local repository => The folder in GitHub 
+*remote repository => The folder in our pc 
+*Upload to GitHub => push
+*download from GitHub => pull
+ git remote add <a name for remote repository>  & URL of repository => uploding remote repository by add a link between remote & local repository
+ git remote => shows above repository which is done or not 
+ git git -v => fot more detail  v => verbos(give more details)
+ git remote remove < the name of remote repository> => remove the remote repository
+
+ ------------push------
+
+
+
+
+
+
+
+
+
+
+
 
  
 */
@@ -29,7 +52,8 @@ git restore ./ _A/ <file name> => wipped out the modified file/ unstaged file.
 git branch => shows all branch and which branch is active at the moment
 git checkout <inactive branch> =>swop in the inactive branch
 git merge <the branch name> merge branch with the master branch
-
+git rm <filename> => removed from file and seystem 
  
-
+git branch -d <branch name> =>delete branch
 */
+
