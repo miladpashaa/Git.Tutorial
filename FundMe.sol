f5faf4f48dc6664787d551dfd5453c8fc8d69c79
milad pasha <miladzanos@gmail.com>
2024-03-24 23:27:29 +0330

    fundnew01

--- FundMe.sol
+++ FundMe.sol
@@ -1,14 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
+
+error InsufficientGas();
 contract fundme{
-    uint public minimumdonate;
-    address public owner;
+    uint public MinimumDonate;
+    address payable public owner;
+    address [] public immutable owners;
+    mapping(address => uint256) public AmountFund;
+
+ constructor(){
+   owners = payable(msg.sender);
+ }
+
+  modifier _Fund(){
+    if(msg.value < 0.8 eth){revert InsufficientGas();}
+  }
  
- function getprice(uint _minimum)public returns(uint){
+ function getprice(uint _minimum)public payable InSufficientGas returns(uint){
 
-    require(msg.vslue < 0.2,"not enough donate");
-
-
-
+   owner.transfer(msg.value);
+   MinimumDonate = _minimum;
+   return minimum;
  }
 }
